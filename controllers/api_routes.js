72b215fc970278d6b3cf21c56a0a7952c5534305
Shipra <drshipradyundi@gmail.com>
2020-01-17 19:14:12 -0800

    moved routes to htmlroutes

--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -46,25 +46,6 @@ router.get("/api/user_data", function(req, res) {
         });
     }
 })
-router.get("/", function(req, res) {
-    console.log("route get /")
-        // If the user already has an account send them to the members page
-        //  if (req.user) {
-        //  res.redirect("/members");
-        // }
-    res.render("signup");
-});
-router.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-        res.redirect("/members");
-    }
-    res.render("login");
-});
-// Here we've add our isAuthenticated middleware to this route.
-// If a user who is not logged in tries to access this route they will be redirected to the signup page
-router.get("/members", isAuthenticated, function(req, res) {
-        res.render("members");
-    })
-    // export routes for server.js to use
+
+// export routes for server.js to use
 module.exports = router;
--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -7,4 +7,25 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
+router.get("/signup", function(req, res) {
+    console.log("route get /")
+        // If the user already has an account send them to the members page
+        //  if (req.user) {
+        //  res.redirect("/members");
+        // }
+    res.render("signup");
+});
+router.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+        res.redirect("/members");
+    }
+    res.render("login");
+});
+// Here we've add our isAuthenticated middleware to this route.
+// If a user who is not logged in tries to access this route they will be redirected to the signup page
+router.get("/members", isAuthenticated, function(req, res) {
+    res.render("members");
+})
+
 module.exports = router;
