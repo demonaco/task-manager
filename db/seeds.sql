218e036a201f1b4ca39958cf7b04d8b7481fe074
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-19 15:37:17 -0800

    initial project tasks page
    * create project status page template.
    * add sample tasks to seeds.db.
    * add helper function to server.js.
    * add route to display project status page.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Projects (title, description, createdAt, updatedAt) VALUES ("Work Tasks", "Daily list of office-related tasks", "2020-01-01", "2020-01-01");
+
+INSERT INTO Tasks (title, description, due_date, status, createdAt, updatedAt, ProjectId) VALUES ("Follow up with Ben", "Ask about availability for Thailand trip", "2020-02-03", "to-do", "2020-01-17", "2020-01-17", "1");
+INSERT INTO Tasks (title, description, due_date, status, createdAt, updatedAt, ProjectId) VALUES ("Reply marketing email", "Ask about partnership opps", "2020-01-30", "in-progress", "2020-01-17", "2020-01-17", "1");
+INSERT INTO Tasks (title, description, due_date, status, createdAt, updatedAt, ProjectId) VALUES ("Project plan", "Write documentation on software project", "2020-02-08", "done", "2020-01-17", "2020-01-17", "1");
