3241875975ff069c64decc10b3d256d04ea1a00d
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-20 18:00:16 -0800

    remove console logs from create new project form

--- public/js/new-project.js
+++ public/js/new-project.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-    console.log("hey, i'm some clientside javascript")
     $("#submit-btn").on("click", function (event) {
         event.preventDefault();
-        console.log("I was clicked!")
+
         var newProject = {
             title: $("#title").val().trim(),
             description: $("#description").val().trim()
@@ -12,7 +11,7 @@ $(document).ready(function () {
             type: "POST",
             data: newProject
         }).then(function () {
-            console.log("server responded to my request")
+
             window.location.replace("/projects");
         })
     })
