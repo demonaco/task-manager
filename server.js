6831083a9b84b12086b27a7f64a2fa5f0e9f8731
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-24 12:16:58 -0800

    include due date in tasks and add helper function to support it

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var session = require("express-session");
 var passport = require("./config/passport");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var moment = require('moment');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -23,12 +24,16 @@ app.use(passport.session());
 app.engine("handlebars", exphbs({
     defaultLayout: "main",
     helpers: {
-        equal: function(lvalue, rvalue, options) {
+        equal: function (lvalue, rvalue, options) {
             if (lvalue != rvalue) {
                 return options.inverse(this);
             } else {
                 return options.fn(this);
             }
+        },
+
+        readableDate: function (date) {
+            return moment(date).format('YYYY-MM-DD');
         }
     }
 }));
@@ -41,8 +46,8 @@ var htmlRoutes = require("./controllers/html_routes.js");
 app.use(htmlRoutes);
 
 
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
 });
