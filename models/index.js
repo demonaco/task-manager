4950081e50550cd58f9cfc645bdda73b1ecf1d1a
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-26 18:17:04 -0800

    revert last commit

--- models/index.js
+++ models/index.js
@@ -9,9 +9,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else if (config.use_env_variable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
